87cbdbca271ef3252c50df9d8377fa4f3c4a8975
Sally Schaffner <schaffners@vpcc.edu>
2024-12-31 16:20:05 -0500

    Operator overloading

--- CSC 223 M1 Examples.cpp
+++ CSC 223 M1 Examples.cpp
@@ -1,10 +1,27 @@
-// CSC 223 M1 Examples.cpp : This file contains examples for the Review of Object-Oriented Programming Module
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Review of OOP\n";
+class Complex {
+private:
+    double real, imag;
+
+public:
+    Complex(double r, double i) : real(r), imag(i) {}
+
+    // Overloading the '+' operator
+    Complex operator+(const Complex& c) {
+        return Complex(real + c.real, imag + c.imag);
+    }
+
+    void display() const {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex c1(1.0, 2.0), c2(2.5, 3.5);
+    Complex c3 = c1 + c2; // Using overloaded '+' operator
+    c3.display();
+
+    return 0;
 }
-
