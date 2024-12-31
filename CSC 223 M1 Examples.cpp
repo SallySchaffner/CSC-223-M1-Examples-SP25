e20af0e7ce89b92c9d6e034d1ab18a945ffa1de6
Sally Schaffner <schaffners@vpcc.edu>
2024-12-31 16:28:14 -0500

    Interface

--- CSC 223 M1 Examples.cpp
+++ CSC 223 M1 Examples.cpp
@@ -1,10 +1,38 @@
-// CSC 223 M1 Examples.cpp : This file contains examples for the Review of Object-Oriented Programming Module
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Review of OOP\n";
+// Define the interface
+class Printable {
+public:
+    virtual void print() const = 0; // Pure virtual function
+    virtual ~Printable() {}
+};
+
+// Implement the interface in a class
+class Document : public Printable {
+public:
+    void print() const override {
+        cout << "Printing Document..." << endl;
+    }
+};
+
+// Implement the interface in another class
+class Image : public Printable {
+public:
+    void print() const override {
+        cout << "Printing Image..." << endl;
+    }
+};
+
+int main() {
+    Printable* doc = new Document();
+    Printable* img = new Image();
+
+    doc->print(); // Output: Printing Document...
+    img->print(); // Output: Printing Image...
+
+    delete doc;
+    delete img;
+
+    return 0;
 }
-
