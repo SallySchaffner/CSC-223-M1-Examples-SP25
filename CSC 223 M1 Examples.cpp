09c085e9936930d32528e514b24f082eb71c936f
Sally Schaffner <schaffners@vpcc.edu>
2024-12-31 16:18:24 -0500

    Function overloading

--- CSC 223 M1 Examples.cpp
+++ CSC 223 M1 Examples.cpp
@@ -1,10 +1,26 @@
-// CSC 223 M1 Examples.cpp : This file contains examples for the Review of Object-Oriented Programming Module
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Review of OOP\n";
+class Printer {
+public:
+    void print(int x) {
+        cout << "Printing integer: " << x << endl;
+    }
+
+    void print(double x) {
+        cout << "Printing double: " << x << endl;
+    }
+
+    void print(const string& x) {
+        cout << "Printing string: " << x << endl;
+    }
+};
+
+int main() {
+    Printer p;
+    p.print(42);            // Calls the integer version
+    p.print(3.14);          // Calls the double version
+    p.print("Hello C++");   // Calls the string version
+
+    return 0;
 }
-
