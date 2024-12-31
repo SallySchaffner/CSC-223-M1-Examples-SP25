4cbbec8f7980369663c85f7ce0cd612c22667c95
Sally Schaffner <schaffners@vpcc.edu>
2024-12-31 16:23:28 -0500

    Compile time binding

--- CSC 223 M1 Examples.cpp
+++ CSC 223 M1 Examples.cpp
@@ -1,10 +1,23 @@
-// CSC 223 M1 Examples.cpp : This file contains examples for the Review of Object-Oriented Programming Module
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Review of OOP\n";
+class CompileTimeExample {
+public:
+    // Function overloading (compile-time binding)
+    void display(int x) {
+        cout << "Integer: " << x << endl;
+    }
+
+    void display(double x) {
+        cout << "Double: " << x << endl;
+    }
+};
+
+int main() {
+    CompileTimeExample obj;
+
+    obj.display(5);      // Calls the version with int parameter
+    obj.display(5.5);    // Calls the version with double parameter
+
+    return 0;
 }
-
